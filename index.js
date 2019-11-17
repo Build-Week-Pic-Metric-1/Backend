f290ef1271bc8209a3658fd22234d547479f1a00
John  Shoff <w.m.t.b5115@gmail.com>
2019-11-17 17:24:35 -0500

    Port fix for Heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 const server = require('./api/server');
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => console.log(`Server is running.. \nDomain: localhost\nPort: ${port}\nInformation: Checkout / for api endpoint documentation.`));
