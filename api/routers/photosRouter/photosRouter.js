33ee10638bdb5830ad45403997ab065ebe1b1dd5
John  Shoff <w.m.t.b5115@gmail.com>
2019-11-19 15:07:24 -0500

    test communication with flask

--- api/routers/photosRouter/photosRouter.js
+++ api/routers/photosRouter/photosRouter.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const authorized = require('../../middleware/authorized');
+const axios = require('axios');
 
 router.get('/:id', authorized, (req,res) =>{
     const {id} = req.params;
@@ -14,9 +15,15 @@ router.put('/:id', authorized, (req,res) =>{
 
 router.post('/:id', authorized, (req,res) =>{
     const {id} = req.params;
-    const {title, url} = req.body;
+    const {photo_id, title, url} = req.body;
 
     if(title && body){
+        axios.post('https://pic-metric1.herokuapp.com/predictor', {photo_id, url})
+        .then(result =>{
+            res.status(200).json(result);
+        }).catch(error =>{
+            res.status(500).json({error: `Error: ${error}`});
+        });
         res.status(201).json({message: `Received a request from user with id: ${id} to add ${title} from url: ${url}`});
     }else{
         res.status(401).json({message: 'Please provide a title & url in your request.'});
