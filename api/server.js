b96f641ae2646430e724971884a9e65875135847
John Shoff <w.m.t.b5115@gmail.com>
2019-11-18 23:23:58 -0500

    Attempting to set origin for cors configuration

--- api/server.js
+++ api/server.js
@@ -20,9 +20,15 @@ const photosRouter = require('./routers/photosRouter/photosRouter');
 // Define server
 const server = express();
 
+const corsOptions = {
+    origin: "https://pic-metric.herokuapp.com",
+    optionsSuccessStatus: 200
+}
+
+
 //Global Middleware 
 server.use(express.json());
-server.use(cors());
+server.use(cors(corsOptions));
 server.use(helmet());
 server.use(morgan('dev'));
 server.use(session(sessionConfig));
@@ -32,7 +38,7 @@ server.get('/api/', (req,res) =>{
     res.status(200).json({message: "Sessions implemented and live!", session: req.session});
 });
 
-server.use('/api/auth', cors(), authRouter);
+server.use('/api/auth', authRouter);
 server.use('/api/photos', photosRouter);
 
 // /* STATIC ENDPOINTS */
